d63c30fd9b59e31a1b4d4ed5f7d3a1d926df01bb
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-07-01 18:12:12 -0300

    trying yo fix cors 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,10 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                source: [
+                    "/api/:path*",
+                    "https://inc-tracker.vercel.app/:path*"
+                ],
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
