8c1211c6e0ad0ec6a0ffe911f95cdbc0d513b641
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-07-01 18:22:17 -0300

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: [
-                    "/api/:path*",
-                    "https://inc-tracker.vercel.app/:path*"
-                ],
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
