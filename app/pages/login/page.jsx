a27d15587de80a4a33ffd8e8f7837a3b9dc820b4
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-07-01 13:37:44 -0300

    modify cors

--- app/pages/login/page.jsx
+++ app/pages/login/page.jsx
@@ -6,7 +6,9 @@ import { redirect } from "next/navigation";
 export default async function Login() {
     const session = await getServerSession(authOptions);
 
-    if (session) redirect("/pages/dashboard");
+    if (session) {
+        redirect("/pages/dashboard");
+    }
 
     return <LoginForm />
 }
