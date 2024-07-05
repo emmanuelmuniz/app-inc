1752587fc706d2b6bd6a83cfa5e470edeb5bf8c4
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2024-07-05 18:11:55 -0300

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className={"bg-ghost-white " + inter.className}>
       <body className="bg-ghost-white min-h-80">
+        <SpeedInsights />
         <SessionProviderClientComponent session={session}>
           <div className="max-w-5xl mx-auto p-2">
             <NavbarApp />
